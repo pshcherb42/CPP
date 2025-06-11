c8f4a796f8ff9f5ec8e9a78aeea8e8b4ddaee825
Polina Shcherbina <ivanavnatattoo@gmail.com>
2025-06-11 19:15:30 +0200

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,2 @@
+CC = c++
+FLAGS = -Wall -Wextra -Werror -std=c++98
---
+++ ex00/ex00.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex00.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pshcherb <pshcherb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 16:22:10 by pshcherb          #+#    #+#             */
+/*   Updated: 2025/06/11 16:26:31 by pshcherb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(char **argv, int argc)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
+    
+}
