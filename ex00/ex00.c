beaad6360a164171b03a49e0775ccf2b820cc887
Polina Shcherbina <pshcherb@car13s3.42barcelona.com>
2025-06-25 21:31:57 +0200

    pup

--- ex00/ex00.c
+++ ex00/ex00.c
@@ -1,23 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ex00.cpp                                           :+:      :+:    :+:   */
+/*   ex00.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pshcherb <pshcherb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 16:22:10 by pshcherb          #+#    #+#             */
-/*   Updated: 2025/06/11 16:26:31 by pshcherb         ###   ########.fr       */
+/*   Updated: 2025/06/25 21:15:28 by pshcherb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cctype>
+#include <stdio.h>
+#include <ctype.h>
 
-int main(char **argv, int argc)
+int main(int argc, char **argv) 
 {
+    int     i;
+    int     j;
+
+    i = 1;
+    (void)argv;
     if (argc == 1)
     {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        printf("* LOUD AND UNBEARABLE FEEDBACK NOISE *\n");
+        return (0);
     }
-    
+    else
+    {
+        while (i < argc)
+        {
+            j = 0;
+            while (argv[i][j])
+            {
+                printf("%c", toupper(argv[i][j]));
+                j++;
+            }
+            i++;
+        }
+        printf("\n");
+    }
 }
